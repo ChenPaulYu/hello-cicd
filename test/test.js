45753375261ab55ff0261c416846120a5890624f
Paul <b10207146@mail.ntust.edu.tw>
2017-09-03 05:33:04 +0800

    change test.js

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var assert = require('assert')
 describe('Array',function(){
 	describe('#indexOf',function(){
 		it('Return -1 if element can not be searched',function(){
-			assert.equal(-1,[1,2,3].indexOf(0))
+			assert.equal(0,[1,2,3].indexOf(0))
 		})
 	})
 })
