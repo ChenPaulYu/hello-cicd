173feae27d5a4741df5ad4e5951446ca031b0cb8
Paul <b10207146@mail.ntust.edu.tw>
2017-09-03 05:02:29 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+
+app.get('/',function(req,res){
+	res.send('Hello World')
+})
+
+
+var server = app.listen(8081,function(){
+	var port = server.address().port
+	console.log('server is listening at port: ' + port)
+})
